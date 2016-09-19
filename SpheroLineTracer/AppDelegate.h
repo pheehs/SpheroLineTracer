3eb28ca0e10ef0ee2e47c2688d391ba5d58846fb
pheehs <ring0pycode@gmail.com>
2016-09-19 14:02:28 +0900

    Initial Commit

---
+++ SpheroLineTracer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpheroLineTracer
+//
+//  Created by Shuhei Horiguchi on 2016/09/19.
+//  Copyright © 2016年 Robohan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SpheroLineTracer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SpheroLineTracer
+//
+//  Created by Shuhei Horiguchi on 2016/09/19.
+//  Copyright © 2016年 Robohan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
